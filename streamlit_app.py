469bf57b4cac45c3c9c202df857d6b3325838546
ruta-github <132392533+ruta-github@users.noreply.github.com>
2023-05-04 03:34:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"watermelon")
+
 
 # take the json version of response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
